6197c47aeaaeb6d8d61104b46460137735cc0dd8
Akaifox16 <kamonpat.sun@outlook.com>
2022-03-18 23:46:34 +0700

    kernel filter

--- convolution.py
+++ convolution.py
@@ -3,13 +3,18 @@ import numpy as np
 def convolution(img, kernel):
     n = len(img)
     m =len(kernel) - 1
+    x = m - 1
     convo = np.array([[0]*(n-m) for _ in range(n-m)])
     kernel_flat = kernel.flatten()
-    for i in range(2, n-2):
-        for j in range(2, n-2):
-            a = img[i-2:i+3, j-2:j+3]
-            convo[i-2][j-2] = convolute(a.flatten(), kernel_flat)
+    for i in range(x, n-x):
+        for j in range(x, n-x):
+            a = img[i-x:i+m, j-x:j+m]
+            convo[i-x][j-x] = convolute(a.flatten(), kernel_flat)
     return convo
+
+def filterFreqDomain(fourier, kernelFourier):
+    return fourier * kernelFourier
+
 
 def convolute(arr_flat, kernel_flat):
     sum_flat = arr_flat * kernel_flat
