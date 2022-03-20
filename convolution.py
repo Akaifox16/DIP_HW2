719f20d511c8692d3565d9f2ab37d3c165894d13
Akaifox16 <kamonpat.sun@outlook.com>
2022-03-20 14:31:05 +0700

    ideal low pass, gaussian low pass, median filter

--- convolution.py
+++ convolution.py
@@ -1,4 +1,5 @@
 import numpy as np
+from dft import fft
 
 def convolution(img, kernel):
     n = len(img)
@@ -15,7 +16,27 @@ def convolution(img, kernel):
 def filterFreqDomain(fourier, kernelFourier):
     return fourier * kernelFourier
 
-
 def convolute(arr_flat, kernel_flat):
     sum_flat = arr_flat * kernel_flat
     return sum_flat.sum()
+
+def cutOff(i, j, n):
+    return np.sqrt((j -n/2)**2 + (i- n/2)**2)
+
+def idealLPfilter(fourier, cutFreq):
+    n = len(fourier)
+    kernel = [[1.0 if cutOff(i,j,n) <= float(cutFreq) else 0.0 for j in range(n)] for i in range(n)]
+    return filterFreqDomain(fourier, np.array(kernel))
+
+def gaussianLPfilter(fourier, cutFreq):
+    n =len(fourier)
+    kernel = [[ np.exp(-0.5 * (cutOff(i, j, n)/ cutFreq) ** 2) for j in range(n)] for i in range(n)]
+    return filterFreqDomain(fourier, np.array(kernel))
+
+def medianfilter(fourier):
+    kernel= np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]) * (1/9)
+    return convolution(np.pad(fourier,1), kernel)
+
+def rms(img, img2):
+    summation = (img.flatten() - img2.flatten()) ** 2
+    return np.sqrt(summation.flatten().mean())
